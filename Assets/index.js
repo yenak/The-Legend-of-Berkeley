2b55448221081abd0666d0a4f9e194b13a9eec48
Yena Kim <yenakim@berkeley.edu>
2017-10-07 17:07:31 -0700

    the legend is complete... for now

--- Assets/index.js
+++ Assets/index.js
@@ -7,12 +7,12 @@ import Expo from 'expo';
 // Map of asset names to modules. List your assets here.
 const modules = {
   'player-sprite': require('./calhacksbear.png'),
-  'pipe-top': require('./pillar-2.png'),
-  'start-screen': require('./splash.png'),
+  'start-screen': require('./title.png'),
   'flowers': require('./flowers.png'),
   'dirt': require('./dirt.jpg'),
   'berkeley': require('./berkeley.jpg'),
-  'stanfurd': require('./stanfordtree.png')
+  'stanfurd': require('./stanfordtree.png'),
+  'dirks': require('./dirks.png')
 };
 
 // Export map of asset names to `Expo.Asset` objects.
